675a400a80b6e6cc9ebda080a45886496235284d
Don Humphreys <dhumphreys@tech-projects.com>
2011-08-05 08:32:45 -0400

    Rewrote plugin build behavior to have more control

---
+++ build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# set version
+VERSION=0.0.1
+
+# set file name
+ZIPFILE=coldroute-$VERSION.zip
+
+# remove the existing zip file
+if [ ! -d build ]; then
+    mkdir build
+fi
+rm -f build/$ZIPFILE
+cd src
+
+# load in the basic plugin files
+zip ../build/$ZIPFILE coldroute.cfc index.cfm
+
+# add the core coldroute files
+zip -r ../build/$ZIPFILE lib/
